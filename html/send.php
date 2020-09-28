7b3bbc74a184652067a3310ea567dc3a46d377ab
mastercool <Tmakxm132@>
2020-09-28 23:15:11 +0900

    form mail

--- html/send.php
+++ html/send.php
@@ -3,40 +3,42 @@
 if(isset($_POST['email'])) {
      
     
-	$email_to = "받을 메일주소";
-	$email_subject = "[폼메일] 문의사항입니다.";
+	$email_to = "mastercool@naver.com";
+	$email_subject = "[1:1 안심상담] 문의신청합니다.";
 	$email_subject = '=?UTF-8?B?'.base64_encode($email_subject).'?=';
 
      
      
     function died($error) {
         // your error code can go here
-        echo "<script> alert('메일발송을 실패하였습니다.');";
+        echo "<script> alert('$error');";
 		echo "history.go(-1);";
 		echo "</script>";
         die();
     }
      
     // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['email']) ||
+    if(!isset($_POST['product']) ||
+        !isset($_POST['first_name']) ||
         !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
+        !isset($_POST['email']) ||
+        !isset($_POST['price'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
-     
+
+    $product = $_POST['product']; // required
     $first_name = $_POST['first_name']; // required
-    $email_from = $_POST['email']; // required
     $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
-     
+    $email_from = $_POST['email']; // required
+    $price = $_POST['price']; // required
+
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/';
   if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
     
-  if(strlen($comments) < 2) {
+  if(strlen($price) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
   if(strlen($error_message) > 0) {
@@ -48,11 +50,12 @@ if(isset($_POST['email'])) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
-     
+
+    $email_message .= "대출상품 : ".clean_string($product)."\n\n";
     $email_message .= "이름 : ".clean_string($first_name)."\n\n";
-    $email_message .= "이메일 : ".clean_string($email_from)."\n\n";
     $email_message .= "연락처 : ".clean_string($telephone)."\n\n";
-    $email_message .= "문의사항 : ".clean_string($comments)."\n\n";
+    $email_message .= "이메일 : ".clean_string($email_from)."\n\n";
+    $email_message .= "희망금액 : ".clean_string($price)."\n\n";
      
      
 // create email headers
